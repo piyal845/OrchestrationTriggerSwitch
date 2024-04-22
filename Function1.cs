38c94d7615d89c1903d6f86d274db294dfed863f
jayan4537 <Jayanta.Dutta@towerswatson.com>
2024-04-22 22:04:07 +0530

    made some changes to all please check

--- Function1.cs
+++ Function1.cs
@@ -17,47 +17,67 @@ namespace OrchestrationTriggerSwitch
         public async Task RunOrchestrator(
             [OrchestrationTrigger] TaskOrchestrationContext context)
         {
-            var config =  GetJsonFromBlob();
+            ILogger logger = context.CreateReplaySafeLogger(nameof(RunOrchestrator));
+
+            var config = GetJsonFromBlob();
             string[] activityFunctionNames = config.GetSection("ActivityFunctions").Get<string[]>();
-
-            //IConfiguration config = new ConfigurationBuilder()
-            //    .SetBasePath(Directory.GetCurrentDirectory())
-            //    .AddJsonFile("config.json", optional: false, reloadOnChange: true)
-            //    .Build();
-
-            //string[] activityFunctionNames = config.GetSection("ActivityFunctions").Get<string[]>();
-
             foreach (string activityFunctionName in activityFunctionNames)
             {
                 switch (activityFunctionName)
                 {
                     case "Action1Activity":
-                        await context.CallActivityAsync("Action1Activity", null);
+                        await context.CallActivityAsync("Action1Activity", logger);
                         break;
                     case "Action2Activity":
-                        await context.CallActivityAsync("Action2Activity", null);
+                        await context.CallActivityAsync("Action2Activity", logger);
                         break;
                     case "AllActivity":
-                        await context.CallActivityAsync("Action1Activity", null);
-                        await context.CallActivityAsync("Action2Activity", null);
+                        await context.CallActivityAsync("Action1Activity", logger);
+                        await context.CallActivityAsync("Action2Activity", logger);
+                        await context.CallActivityAsync("Action3Activity", logger);
+                        await context.CallActivityAsync("Action4Activity", logger);
                         break;
                     default:
-                        Console.WriteLine($"Invalid activity function '{activityFunctionName}' specified in configuration.");
+                        logger.LogInformation($"Invalid activity function '{activityFunctionName}' specified in configuration.");
                         break;
                 }
             }
         }
 
         [Function(nameof(Action1Activity))]
-        public  void Action1Activity([ActivityTrigger] TaskContext context)
+        public void Action1Activity([ActivityTrigger] FunctionContext executionContext)
         {
-            Console.WriteLine("Action1 activity executed.");
+            ILogger log = executionContext.GetLogger("Action1Activity");
+            log.LogInformation("Activity1 executed.");
+
+        }
+        [Function(nameof(Action2Activity))]
+        public void Action2Activity([ActivityTrigger] FunctionContext executionContext)
+        {
+            ILogger log = executionContext.GetLogger("Action2Activity");
+            log.LogInformation("Activity2 executed.");
+        }
+        [Function(nameof(Action3Activity))]
+        public void Action3Activity([ActivityTrigger] FunctionContext executionContext)
+        {
+            ILogger log = executionContext.GetLogger("Action3Activity");
+            log.LogInformation("Action3 activity started ......");
+            Thread.Sleep(180000);
+            log.LogInformation("Activity 3 is still running......." + DateTime.Now.ToString());
+            Thread.Sleep(180000);
+            log.LogInformation("Activity 3 is still running......." + DateTime.Now.ToString());
+            Thread.Sleep(180000);
+            log.LogInformation("Activity 3 is still running......." + DateTime.Now.ToString());
+            Thread.Sleep(60000);
+            log.LogInformation("Activity 3 completed it's execution" + DateTime.Now.ToString());
+
         }
 
-        [Function(nameof(Action2Activity))]
-        public  void Action2Activity([ActivityTrigger] TaskContext context)
+        [Function(nameof(Action4Activity))]
+        public void Action4Activity([ActivityTrigger] FunctionContext executionContext)
         {
-            Console.WriteLine("Action2 activity executed.");
+            ILogger log = executionContext.GetLogger("Action4Activity");
+            log.LogInformation("Activity4 executed.");
         }
 
         [Function("Function1_HttpStart")]
@@ -80,9 +100,9 @@ namespace OrchestrationTriggerSwitch
 
         private  static IConfiguration GetJsonFromBlob()
         {
-            var blobServiceClient = new BlobServiceClient("DefaultEndpointsProtocol=https;AccountName=testdurable86ba;AccountKey=PLRr9f7iU4dPGBG3EZ3S2r7QCI7X1B15rJSTCsBw6V/d4f8rwwVe2YFkjpP2KGZIvtCK9L3zEGxQ+AStmhpfZQ==;EndpointSuffix=core.windows.net");
-            var blobContainerClient = blobServiceClient.GetBlobContainerClient("testorchestratorcontainers");
-            var blobClient = blobContainerClient.GetBlobClient("config.json");
+            var blobServiceClient = new BlobServiceClient("<Your_blob_connectionstring>");
+            var blobContainerClient = blobServiceClient.GetBlobContainerClient("<your_blob_container_name>");
+            var blobClient = blobContainerClient.GetBlobClient("<blob_name>");
 
             using var memoryStream = new MemoryStream();
             blobClient.DownloadTo(memoryStream);
